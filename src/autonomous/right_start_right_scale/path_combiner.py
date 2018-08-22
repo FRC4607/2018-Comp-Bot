59f7d2e60ee8701e6856ba8763abc2c44c8adf27
Bevin323 <BenMcBacon@hotmail.com>
2018-08-22 15:18:38 -0500

    Negated position and velocity / feed-forward of reverse paths

--- src/autonomous/right_start_right_scale/path_combiner.py
+++ src/autonomous/right_start_right_scale/path_combiner.py
@@ -1,7 +1,7 @@
 import os
 import pickle
 from wpilib.command import CommandGroup
-from importlib.resources import path
+
 
 with open(os.path.join(os.path.dirname(__file__), 'path.pickle'), "rb") as fp:
     path = pickle.load(fp)          
@@ -13,27 +13,48 @@ with open(os.path.join(os.path.dirname(__file__), 'path3.pickle'), "rb") as fp:
     path3 = pickle.load(fp)
 with open(os.path.join(os.path.dirname(__file__), 'path4.pickle'), "rb") as fp:
     path4 = pickle.load(fp)  
-            
+    
+        
+for i in range(len(path2['left'])):    
+    path2['left'][i][0] = -path2['left'][i][0]
+    path2['left'][i][1] = -path2['left'][i][1] 
+     
+for i in range(len(path2['right'])):    
+    path2['right'][i][0] = -path2['right'][i][0]    
+    path2['right'][i][1] = -path2['right'][i][1]
+     
+for i in range(len(path4['left'])):    
+    path4['left'][i][0] = -path4['left'][i][0]
+    path4['left'][i][1] = -path4['left'][i][1] 
+     
+for i in range(len(path4['right'])):    
+    path4['right'][i][0] = -path4['right'][i][0]    
+    path4['right'][i][1] = -path4['right'][i][1]    
       
 for key in path2:
     for data in (path2[key]):       
-        #print(data)
         path1[key].append(data)
+
+for key in path1:
+    for data in (path1[key]):
+        print(data)
         
 for key in path3:
     for data in (path3[key]):       
-        #print(path2[key][key2])
         path1[key].append(data)
-        
+
 for key in path4:
-    for data in (path4[key]):      
-        #print(path2[key][key2])
-        path1[key].append(data)        
-        
+    for data in (path4[key]):         
+        path1[key].append(data)         
+       
 with open(os.path.join(os.path.dirname(__file__), "best_path.pickle"), "wb") as fp:
-    pickle.dump(path1, fp)       
+    pickle.dump(path1, fp)  
+with open(os.path.join(os.path.dirname(__file__), "best_path.pickle"), "rb") as fp:
+    bestPath = pickle.load(fp)         
+       
+ 
 #===============================================================================
-# print(len(path[key]))
-# print(len(path2[key]))
-# print(len(path1[key]))
+# for key in bestPath:
+#     for data in (bestPath[key]):     
+#         print(data)   
 #===============================================================================
